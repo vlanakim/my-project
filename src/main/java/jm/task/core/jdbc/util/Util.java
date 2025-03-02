ebc8f36b5d44e8d907ebd094cf40b30172e9570e
Vlana Kim <vlanakim@Noutbuk-Vlana.local>
2025-03-02 16:41:38 +0300

    Обновлен класс Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -11,25 +11,22 @@ public class Util {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/my_database";
     private static final String USER = "root";
     private static final String PASSWORD = "imnamekim01";
-
-    public static Connection getConnection() {
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("База данных подключена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return connection;
-    }
-
-    private static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    private static SessionFactory sessionFactory;
 
     private Util() {}
 
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
     public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            synchronized (Util.class) {
+                if (sessionFactory == null) {
+                    sessionFactory = new Configuration().configure().buildSessionFactory();
+                }
+            }
+        }
         return sessionFactory;
     }
 }
